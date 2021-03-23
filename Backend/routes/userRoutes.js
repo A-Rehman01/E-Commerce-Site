ec8d6984f5039451ba98dc37739859d6cdf1e3b5
abdulrehman <arehmana377@gmail.com>
2021-03-23 21:36:17 +0500

    Add Admin MiddleWares

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -4,9 +4,10 @@ import {
   getUserProfile,
   registerUser,
   updateUserProfile,
+  getUsers,
 } from '../controllers/userController.js';
 const router = express.Router();
-import { protect } from '../middlewares/authMiddleware.js';
+import { protect, admin } from '../middlewares/authMiddleware.js';
 
 // @desc    Auth User & get Token
 // @route   POST /api/users/login
@@ -29,5 +30,10 @@ router
   .route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
+
+// @desc    Get all uers
+// @route   GET /api/users
+// @access  Private/Admin
+router.route('/').get(protect, admin, getUsers);
 
 export default router;
